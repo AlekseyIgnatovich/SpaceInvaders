e24843d3ed5f48cadb303915abd4b23bcec38947
Алексей Игнатович <a.ignatovich@social-infinite.com>
2023-06-11 12:42:34 +0600

    instatiate invaders in borders

--- Assets/Scripts/InvadersSpawner.cs
+++ Assets/Scripts/InvadersSpawner.cs
@@ -3,43 +3,44 @@ using Zenject;
 
 public class InvadersSpawner
 {
-	private const float Width = 15f;
-	private const float MaxHeight = 4.5f;
 	private const float RowHeight = 1f;
-	private const float HorizontalOffset = 0.5f;
-	private const int InRowCount = 10;
 	private const int RowsCount = 3;
+	private const float WidthStep = 1.3f;
 
 	private Invader.Factory _invadersFactory;
 	private DataModel _dataModel;
+	private LevelBorders _levelBorders;
 
-	public InvadersSpawner(SignalBus signalBus, Invader.Factory invadersFactory, DataModel dataModel)
+	public InvadersSpawner(SignalBus signalBus,
+		Invader.Factory invadersFactory,
+		DataModel dataModel,
+		LevelBorders levelBorders)
 	{
 		_invadersFactory = invadersFactory;
 		_dataModel = dataModel;
+		_levelBorders = levelBorders;
 
 		signalBus.Subscribe<RestartLevelSignal>(RespawnInvaders);
 	}
 
 	private void RespawnInvaders()
 	{
-		var height = MaxHeight;
-		var widthStep = Width / InRowCount;
-		var leftPos = -Width / 2;
-		var count = 0;
+		var borderOffset = WidthStep / 2;
+		var height = _levelBorders.Height - borderOffset;
+		int inRowCount = (int)(2 * _levelBorders.Width / WidthStep);
+		var leftPos = -WidthStep * inRowCount / 2;
 		for (int i = 0; i < RowsCount; i++)
 		{
-			for (int j = 0; j < InRowCount; j++)
+			for (int j = 0; j < inRowCount; j++)
 			{
-				var pos = new Vector3(HorizontalOffset + leftPos + widthStep * j, height, 0);
+				var pos = new Vector3(borderOffset + leftPos + WidthStep * j, height, 0);
 				var invader = _invadersFactory.Create();
 				invader.transform.position = pos;
-				count++;
 			}
 
 			height -= RowHeight;
 		}
 
-		_dataModel.InvadersCount.Value = count;
+		_dataModel.InvadersCount.Value = RowsCount * inRowCount;
 	}
 }
